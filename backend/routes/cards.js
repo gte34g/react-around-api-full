0e6b1d63e8339e912385182937f2ed8d0b6ae128
Ilya Libin <ilyali911@gmail.com>
2022-10-26 22:23:38 +0300

    fixing routes folder

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -13,10 +13,10 @@ const {
   newCardValidation,
 } = require('../middlewares/validation');
 
-router.get('/cards', authValidation, getCards);
-router.post('/cards', authValidation, newCardValidation, createCard);
-router.put('/cards/:_id/likes', authValidation, cardValidationId, likeCard);
-router.delete('/cards/:_id', authValidation, cardValidationId, deleteCardById);
-router.delete('/cards/:_id/likes', authValidation, cardValidationId, disLikeCard);
+router.get('/', authValidation, getCards);
+router.post('/', authValidation, newCardValidation, createCard);
+router.put('/:_id/likes', authValidation, cardValidationId, likeCard);
+router.delete('/:_id', authValidation, cardValidationId, deleteCardById);
+router.delete('/:_id/likes', authValidation, cardValidationId, disLikeCard);
 
 module.exports = router;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,11 +14,11 @@ const {
   updateAvatarValidation,
 } = require('../middlewares/validation');
 
-router.get('/users', authValidation, getUsers);
-router.get('/users/:_id', authValidation, validateUserdId, getUserById);
-router.get('/users/me', authValidation, getCurrentUser);
+router.get('/', authValidation, getUsers);
+router.get('/:_id', authValidation, validateUserdId, getUserById);
+router.get('/me', authValidation, getCurrentUser);
 
-router.patch('/users/me', authValidation, updateUser);
-router.patch('/users/me/avatar', authValidation, updateAvatarValidation, updateUserValidation, updateAvatar);
+router.patch('/me', authValidation, updateUser);
+router.patch('/me/avatar', authValidation, updateAvatarValidation, updateUserValidation, updateAvatar);
 
 module.exports = router;
