404a7553bfe3f36dbbb4fcbbb5a8885aafe61803
Ilya Libin <ilyali911@gmail.com>
2022-10-29 16:43:36 +0300

    fixing validation file

--- backend/middlewares/validation.js
+++ backend/middlewares/validation.js
@@ -28,6 +28,9 @@ const validateUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().custom(validateEmail),
     password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom(validateUrl),
   }),
 });
 
@@ -35,6 +38,9 @@ const validateLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().custom(validateEmail),
     password: Joi.string().required(),
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().custom(validateUrl),
   }),
 });
 
